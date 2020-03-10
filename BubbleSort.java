be73dfe18bc4bc90debf5e661b5c83625ac8d3d1
James Fyffe <s3782619@student.rmit.edu.au>
2020-03-10 21:54:49 +1100

    updated test code

--- BubbleSort.java
+++ BubbleSort.java
@@ -12,7 +12,7 @@ public class Main {
         for (int i=0; i<n-1; i++){
             for (int j=0; j<n-1; j++){
                 if (SortableItems[j] > SortableItems[j+1]){
-                    TEST CODE TEST CODE
+                    TEST CODE TEST CODE test code
                 }
             }
 
