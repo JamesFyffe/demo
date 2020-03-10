18d1a5f7352fe934ff67a4296c009a91e03b548d
[james <[s3782619@student.rmit.edu.au]>
2020-03-10 18:19:45 +1100

    test

---
+++ BubbleSort.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args){
+        int[] items = new int[5];
+        BubbleSort(items);
+
+    }
+
+    public static void BubbleSort(int[] SortableItems){
+        int n = SortableItems.length;
+
+        for (int i=0; i<n-1; i++){
+            for (int j=0; j<n-1; j++){
+                if (SortableItems[j] > SortableItems[j+1]){
+                    TEST CODE TEST CODE
+                }
+            }
+
+        }
+    }
+
+    private void swap(int a, int b){
+        ;
+    }
+}
